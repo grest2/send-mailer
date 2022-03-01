78cdd6df0b6bd499f818d3047c46cfc586c6bbd8
login <polyanaozero@gmail.com>
2022-03-01 16:44:07 +0300

    send-mailer
    init

---
+++ SendMailer/SendMailer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SendMailer
+//
+//  Created by iOS Developer on 3/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
